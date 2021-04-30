8971e727b71ae8a9af0170e5a88c8cd45929d53c
dofuuz <dofu@nate.com>
2021-05-01 00:12:40 +0900

    Refactoring, Add comment

--- gen_wheel.py
+++ gen_wheel.py
@@ -57,6 +57,7 @@ CCW = False
 SMOOTH = 1/3
 
 
+# Generate CAM02-UCS(Jp, ap, bp) colorspace
 j_space = np.linspace(0.1, 100, J_RES)
 c_space = np.linspace(0, 50, C_RES)
 
@@ -65,7 +66,6 @@ if CCW:
 else:
     h_ = np.linspace(OFFSET, ANGLE+OFFSET, J_RES)
 
-# Generate Jpapbp colorspace
 jpapbp = np.zeros([C_RES, J_RES, 3])
 for jdx, jp in enumerate(j_space):
     for cdx, chroma in enumerate(c_space):
@@ -75,6 +75,7 @@ for jdx, jp in enumerate(j_space):
 
 # Convert to sRGB
 rgb = colorspacious.cspace_convert(jpapbp, "CAM02-UCS", "sRGB1")
+
 
 # Get chroma limit of sRGB
 c_limit = np.zeros_like(j_space)
@@ -88,16 +89,8 @@ for jdx in range(J_RES):
     c_limit[jdx] = max_cdx
 
 
-# Generate contour
-# b = C_RES * 3 / 4 * 0.9
-# d = np.linspace(0, 100, J_RES)
-# c_smoothed = -b/(50**2) * (d-50)**2 + b
-
-
-# Smooth contour
+# Smooth chroma limit contour
 c_smoothed = np.concatenate([-c_limit[::-1][:-1], c_limit, -c_limit[::-1][1:]])
-
-# c_smoothed = filters.uniform_filter1d(c_smoothed, int(J_RES*SMOOTH))
 
 c_smoothed = savgol_filter(c_smoothed, math.ceil(J_RES*SMOOTH*1.5/2)*2 - 1, 3)
 c_smoothed = filters.uniform_filter1d(c_smoothed, int(J_RES*SMOOTH*1.5/2)) * 0.95
@@ -106,37 +99,38 @@ c_smoothed = c_smoothed[J_RES:2*J_RES]
 
 c_selected = c_smoothed.clip(min=0).astype(int)
 
-# Generate gaumt image
+
+# Generate and plot gaumt
 gamut_image = np.copy(rgb)
 gamut_image[gamut_image<=0] = 1
 gamut_image[1<gamut_image] = 0
 
-# Select color, and mark on image
-cm_data = []
+# Mark smoothed contour on image
 for jdx, max_c in enumerate(c_selected):
     if 0 == jdx % 2:
         gamut_image[max_c, jdx] = 1
     else:
         gamut_image[max_c, jdx] = 0
 
-
-c_real = c_smoothed * 50 / C_RES
-
-cm_data = []
-for jdx, chroma in enumerate(c_real):
-    jp = j_space[jdx]
-    ap = np.cos(h_[jdx]) * chroma
-    bp = np.sin(h_[jdx]) * chroma
-
-    cm_data.append([jp, ap, bp])
-
-cm_data = colorspacious.cspace_convert(cm_data, "CAM02-UCS", "sRGB1")
-cm_data = np.clip(cm_data, 0, 1)
-
 plt.figure(figsize=[10, 10])
 plt.imshow(gamut_image)
 
 
+# Get colors on contour
+cm_jpapbp = []
+for jdx, cdx in enumerate(c_smoothed):
+    chroma = cdx * 50 / C_RES
+    jp = j_space[jdx]
+    ap = np.cos(h_[jdx]) * chroma
+    bp = np.sin(h_[jdx]) * chroma
+
+    cm_jpapbp.append([jp, ap, bp])
+
+cm_data = colorspacious.cspace_convert(cm_jpapbp, "CAM02-UCS", "sRGB1")
+cm_data = np.clip(cm_data, 0, 1)
+
+
+# Display viscm
 test_cm = ListedColormap(cm_data, name=NAME)
 
 try:
@@ -146,6 +140,8 @@ except ImportError:
     print("viscm not found, falling back on simple display")
     plt.imshow(np.linspace(0, 100, 256)[None, :], aspect='auto', cmap=test_cm)
 
+
+# Plot RGB value graph
 cm255 = np.asarray(cm_data) * 255
 seg_simple = 8
 
@@ -154,14 +150,15 @@ plt.plot(cm255[:,0], 'r')
 plt.plot(cm255[:,1], 'g')
 plt.plot(cm255[:,2], 'b')
 plt.plot(np.mean(cm255, axis=1))
-# plt.plot(np.arange(0, 257, 256/seg_simple), np.asarray(cm_simplified)*256)
-# plt.plot(np.arange(0, 257, 256/seg_simple), np.mean(np.asarray(cm_simplified)*256, axis=1))
-ax.set_xticks(np.linspace(0, J_RES, seg_simple, endpoint=False))
+
+ax.set_xticks(np.linspace(0, J_RES, seg_simple+1, endpoint=True))
 ax.set_yticks(np.arange(0, 257, 16))
 
 ax.grid(which='both')
 plt.show()
 
+
+# Save colormap to C format
 cm_data_u8 = (cm_data*255 + 0.5).astype('uint8')
 with open('AColorResources.h', 'wt') as output_file:
     print('const unsigned char spectroGradient[512][3] = {', file=output_file)
